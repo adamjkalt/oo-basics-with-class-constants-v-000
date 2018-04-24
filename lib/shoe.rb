6bbff88788e3fb85576e30a8c9b537ae60808bae
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-24 17:25:20 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,7 @@ class Shoe
   attr_reader :brand
 
   BRANDS []
-  
+
   def initialize(brand)
     @brand = brand
   end
@@ -13,25 +13,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-end
-
-class Book
-  attr_accessor :author, :page_count  # remove the attr_accessor for genre
-  attr_reader :title, :genre  # add an attr_reader for genre
-
-  GENRES = []
-
-  def initialize(title)
-    @title = title
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
   end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-
-  # create the writer for genre and add the logic for the class constant
-  def genre=(genre)
-    @genre = genre
-    GENRES << genre
-  end
 end
